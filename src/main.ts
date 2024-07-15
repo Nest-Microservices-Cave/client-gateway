f0ca25113594641d8d6c8f30baaf7be5371d7386
Josue Ordonez <razuordonez@gmail.com>
2024-07-15 17:44:22 -0500

    changes

--- src/main.ts
+++ src/main.ts
@@ -21,6 +21,7 @@ async function bootstrap() {
   app.useGlobalFilters(new RpcCustomExceptionFilter());
 
   await app.listen(envs.port);
+
   logger.log(`Gateway running on port ${envs.port}`);
 }
 bootstrap();
